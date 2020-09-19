01d4d26761e90a0b795e089a30152c1d75afede9
finchy10 <crystalfinch11@gmail.com>
2020-09-19 10:15:41 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,11 @@ $(function() {
         }
     });
 }); 
+
+$("#loginButton").click(function() {
+    $('#loginModal').modal("show");
+    })
+
+$("#reserveButton").click(function() {
+    $('#reserveModal').modal("show");
+    })
